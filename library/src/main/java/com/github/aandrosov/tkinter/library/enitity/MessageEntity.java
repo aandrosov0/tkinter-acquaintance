efe6a1acdff7d43658dbcbd72ae735b55f221c56
aandrosov0 <aandrosov338@gmail.com>
2023-07-14 16:59:57 +0300

    Add body of class MessageEntity.java

--- library/src/main/java/com/github/aandrosov/tkinter/library/enitity/MessageEntity.java
+++ library/src/main/java/com/github/aandrosov/tkinter/library/enitity/MessageEntity.java
@@ -1,5 +1,53 @@
 package com.github.aandrosov.tkinter.library.enitity;
 
-public class MessageEntity {
-    //TODO
+import com.google.gson.annotations.SerializedName;
+
+public class MessageEntity extends Entity {
+
+    @SerializedName("author_id")
+    private long authorId;
+
+    @SerializedName("destination_id")
+    private long destinationId;
+
+    private String text;
+
+    public MessageEntity() {
+        super(0);
+    }
+
+    public MessageEntity(long id, long authorId, long destinationId, String text) {
+        super(id);
+        this.authorId = authorId;
+        this.destinationId = destinationId;
+        this.text = text;
+    }
+
+    public MessageEntity(long authorId, long destinationId, String text) {
+        this(0, authorId, destinationId, text);
+    }
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+
+    public long getDestinationId() {
+        return destinationId;
+    }
+
+    public void setDestinationId(long destinationId) {
+        this.destinationId = destinationId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
